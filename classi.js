1443d18572271082b5d3314e2d1cd5828ac0ed90
itsemanuela <notemanu152@gmail.com>
2026-04-13 17:14:04 +0200

    commit

---
+++ classi.js
@@ -0,0 +1,12 @@
+const user = function (_firstName, _lastName, _age, _location) {
+  this.firstName = _firstName;
+  this.lastName = _lastName;
+  this.age = _age;
+  this.location = _location;
+};
+user();
+return "Ciao, mi chiamo ${this.firstName} , ${this.lastname";
+user();
+console.log("ciao mi chiamo ${this.firstname}, ${this.lastName");
+("${}");
+${}
